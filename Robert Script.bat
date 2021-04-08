614a399a5f3bb7dad3ab5b08e7593356f7057f9a
Robert Conde <rconde2003@gmail.com>
2021-04-08 08:58:49 -0400

    Update Robert Script.bat

--- Robert Script.bat
+++ Robert Script.bat
@@ -2,6 +2,6 @@ RMDIR /Q/S "C:\Users\rcond\Documents\curseforge\minecraft\Instances\Multiblock\c
 RMDIR /Q/S "C:\Users\rcond\Documents\curseforge\minecraft\Instances\Multiblock\scipts"
 RMDIR /Q/S "C:\Users\rcond\Documents\curseforge\minecraft\Instances\Multiblock\resources"
 
-Xcopy \i "C:\Users\rcond\Documents\GitHub\Aider-Festag\overrides\config" "C:\Users\rcond\Documents\curseforge\minecraft\Instances\Multiblock\config"
-Xcopy \i "C:\Users\rcond\Documents\GitHub\Aider-Festag\overrides\scripts" "C:\Users\rcond\Documents\curseforge\minecraft\Instances\Multiblock\scipts"
-Xcopy \i "C:\Users\rcond\Documents\GitHub\Aider-Festag\overrides\resources" "C:\Users\rcond\Documents\curseforge\minecraft\Instances\Multiblock\resources"
+XCOPY /I /S "C:\Users\rcond\Documents\GitHub\Aider-Festag\overrides\config" "C:\Users\rcond\Documents\curseforge\minecraft\Instances\Multiblock\config"
+XCOPY /I /S "C:\Users\rcond\Documents\GitHub\Aider-Festag\overrides\scripts" "C:\Users\rcond\Documents\curseforge\minecraft\Instances\Multiblock\scipts"
+XCOPY /I /S "C:\Users\rcond\Documents\GitHub\Aider-Festag\overrides\resources" "C:\Users\rcond\Documents\curseforge\minecraft\Instances\Multiblock\resources"
